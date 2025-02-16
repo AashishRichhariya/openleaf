8ff16f7484df62466bf16b84f4394011c5ae2ae5
AashishRichhariya <32592885+AashishRichhariya@users.noreply.github.com>
2025-02-16 21:14:03 +0530

    Added lexical

---
+++ app/editor.tsx
@@ -0,0 +1,36 @@
+import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { ReactElement } from "react";
+
+const theme = {
+  // Theme styling goes here
+  //...
+};
+
+function onError(error: Error) {
+  console.error(error);
+}
+
+export default function Editor(): ReactElement {
+  const initialConfig = {
+    namespace: "OpenleafEditor",
+    theme,
+    onError,
+  };
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <RichTextPlugin
+        contentEditable={<ContentEditable />}
+        placeholder={<div>Enter some text...</div>}
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+      <HistoryPlugin />
+      <AutoFocusPlugin />
+    </LexicalComposer>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,7 @@
 "use client";
 
-import { useState } from "react";
+import Editor from "./editor";
 
 export default function EditorPage() {
-  const [content, setContent] = useState("");
-
-  return (
-    <div className="editor-container">
-      <div
-        className="editor-content"
-        contentEditable
-        suppressContentEditableWarning
-        // placeholder="Start writing..."
-        onInput={(e) => {
-          const text = e.currentTarget.textContent || "";
-          setContent(text);
-        }}
-      />
-    </div>
-  );
+  return <Editor />;
 }
