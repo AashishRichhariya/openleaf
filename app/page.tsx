f9f5ef9da4125ca5b04a53a89a6fe68c37a2c306
AashishRichhariya <32592885+AashishRichhariya@users.noreply.github.com>
2025-04-01 23:56:42 +0530

    Added slug based redirection

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
-"use client";
+import { redirect } from "next/navigation";
 
-import Editor from "./components/editor/Editor";
+/**
+ * Generates a random 10-character string
+ * @returns A random 10-character string
+ */
+function getRandomString(): string {
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
 
-export default function EditorPage() {
-  return <Editor />;
+  for (let i = 0; i < 10; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+
+  return result;
+}
+
+export default function Home() {
+  // Generate a random, readable slug
+  const randomSlug = getRandomString();
+  redirect(`/${randomSlug}`);
 }
